e0213cf63bc0ecd4072eb0061cc3d6493d6745ba
JuHo Jeon <juho914@naver.com>
2023-03-21 18:42:59 +0900

    전주호 : 20230321

--- Homework/041_Class003_Struct/041_Class003_Struct.cpp
+++ Homework/041_Class003_Struct/041_Class003_Struct.cpp
@@ -1,13 +1,34 @@
-﻿// 001_AR47Start.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+﻿// 041_Class003_Struct.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
-
-// 코브부분이라고 하는데
 
 #include <iostream>
 
+class Player
+{
+// private: 디폴트 접근제한 지정자
+private:
+    int Att = 10;
+    int Def = 2;
+    int Hp = 100;
+    int Speed = 3;
+};
+
+struct Monster
+{
+// public: 디폴트 접근제한 지정자
+private:
+    int Att = 10;
+    int Def = 2;
+    int Hp = 100;
+    int Speed = 3;
+};
+
 int main()
 {
-    // 안녕하세요
+    Player NewPlayer;
+
+    Monster NewMonster;
+
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
