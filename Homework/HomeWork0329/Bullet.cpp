d951372f22da7e4898960d1b07fc4e9a4c7a10c5
JuHo Jeon <juho914@naver.com>
2023-03-30 04:33:29 +0900

    전주호: HomeWork0329

---
+++ Homework/HomeWork0329/Bullet.cpp
@@ -0,0 +1,35 @@
+#include "Bullet.h"
+#include "Player.h"
+
+Player NewPlayer;
+
+Bullet::Bullet()
+{
+}
+
+void Bullet::ResetPos()
+{
+	Pos = NewPlayer.GetPos();
+}
+
+void Bullet::MovePos()
+{
+	Pos = int2{ Pos.X, Pos.Y - 1 };
+}
+
+void Bullet::SetPos(int2 _pos)
+{
+	Pos = _pos;
+}
+
+//void Bullet::Fire()
+//{
+//	if (true == NewPlayer.IsFire())
+//	{
+//		int2 BulletPos = NewPlayer.GetPos();
+//		Bullet::GetABullet().SetPos(int2(NewPlayer.GetPos().X, NewPlayer.GetPos().Y - 1));
+//	}
+
+
+	//ConsoleGameScreen::GetMainScreen().SetScreenCharacter(NewPlayer.GetPos(), '^');
+//}
---
+++ Homework/HomeWork0329/Bullet.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "ConsoleGameMath.h"
+
+class Player;
+
+class Bullet
+{
+public:
+
+	static Bullet& GetABullet()
+	{
+		static Bullet ABullet;
+		return ABullet;
+	}
+
+	void ResetPos();
+
+	void MovePos();
+
+	void SetPos(int2 _pos);
+
+	inline int2 GetPos() const
+	{
+		return Pos;
+	}
+
+
+private:
+	Bullet();
+
+	int2 Pos = int2(0, 0);
+
+
+};
+
