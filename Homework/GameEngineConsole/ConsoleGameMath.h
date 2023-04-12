500c631c3b151f8cafcffa5a46dbf99c4d47261c
JuHo Jeon <juho914@naver.com>
2023-04-12 10:15:44 +0900

    전주호: HomeWork0411

--- Homework/GameEngineConsole/ConsoleGameMath.h
+++ Homework/GameEngineConsole/ConsoleGameMath.h
@@ -3,22 +3,28 @@
 // 헤더에만 다 만든다.
 class int2
 {
+
 public:
+	static const int2 Left;
+	static const int2 Right;
+	static const int2 Up;
+	static const int2 Down;
+
 	int X = 0;
 	int Y = 0;
 
 public:
-	inline int2 Half()
+	inline int2 Half() const
 	{
 		return { X / 2, Y / 2 };
 	}
 
-	bool Check(const int2& _Other)
+	bool Check(const int2& _Other) const
 	{
 		return X == _Other.X && Y == _Other.Y;
 	}
 
-	bool operator==(const int2& _Other) 
+	bool operator==(const int2& _Other) const
 	{
 		return X == _Other.X && Y == _Other.Y;
 	}
---
+++ Homework/HomeWork0411/Player.cpp
@@ -0,0 +1,126 @@
+#include "Player.h"
+#include <conio.h>
+#include <Windows.h>
+#include <GameEngineConsole/ConsoleGameScreen.h>
+#include "ConsoleObjectManager.h"
+#include "Bomb.h"
+#include "GameEnum.h"
+#include "ConsoleObjectManager.h"
+
+bool Player::IsGameUpdate = true;
+
+Player::Player()
+{
+	RenderChar = '*';
+	SetPos(ConsoleGameScreen::GetMainScreen().GetScreenSize().Half());
+
+}
+
+bool Player::IsBomb(int2 _NextPos)
+{
+	// 폭탄이 설치되었다면 못통과하게 만들어놓으세요.
+	GameEngineArray<ConsoleGameObject*>& BombGroup
+		= ConsoleObjectManager::GetGroup(ObjectOrder::Bomb);
+
+	bool ReturnValue = false;
+
+	for (size_t i = 0; i < BombGroup.Count(); i++)
+	{
+		if (BombGroup[i] == nullptr)
+			continue;
+
+		if (_NextPos == BombGroup[i]->GetPos())
+		{
+			ReturnValue = true;
+		}
+	}
+
+	for (size_t i = 0; i < BombGroup.Count(); i++)
+	{
+		delete BombGroup[i];
+	}
+
+	return ReturnValue;
+}
+
+// 화면바깥으로 못나가게 하세요. 
+void Player::Update()
+{
+	if (0 == _kbhit())
+	{
+		return;
+	}
+
+	char Ch = _getch();
+
+	int2 NextPos = { 0, 0 };
+
+
+	switch (Ch)
+	{
+	case 'a':
+	case 'A':
+		NextPos = Pos;
+		NextPos.X -= 1;
+		if (true == IsBomb(NextPos))
+			break;
+		if (false == ConsoleGameScreen::GetMainScreen().IsScreenOver(NextPos))
+		{
+			Pos.X -= 1;
+		}
+		break;
+	case 'd':
+	case 'D':
+		NextPos = Pos;
+		NextPos.X += 1;
+		if (true == IsBomb(NextPos))
+			break;
+		if (false == ConsoleGameScreen::GetMainScreen().IsScreenOver(NextPos))
+		{
+			Pos.X += 1;
+		}
+		break;
+	case 'w':
+	case 'W':
+		NextPos = Pos;
+		NextPos.Y -= 1;
+		if (true == IsBomb(NextPos))
+			break;
+		if (false == ConsoleGameScreen::GetMainScreen().IsScreenOver(NextPos))
+		{
+			Pos.Y -= 1;
+		}
+		break;
+	case 's':
+	case 'S':
+		NextPos = Pos;
+		NextPos.Y += 1;
+		if (true == IsBomb(NextPos))
+			break;
+		if (false == ConsoleGameScreen::GetMainScreen().IsScreenOver(NextPos))
+		{
+			Pos.Y += 1;
+		}
+		break;
+	case 'f':
+	case 'F':
+	{
+		// 여기 이상황에서 보면
+		// ConsoleObjectManager 있고
+		// Player 
+		Bomb* NewBomb = ConsoleObjectManager::CreateConsoleObject<Bomb>(ObjectOrder::Bomb);
+		NewBomb->Init(BombPower);
+		NewBomb->SetPos(GetPos());
+		// 폭탄설치 
+		break;
+	}
+	case 'q':
+	case 'Q':
+	{
+		IsGameUpdate = false;
+		break;
+	}
+	default:
+		break;
+	}
+}
